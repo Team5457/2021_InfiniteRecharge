766dfce63e0e408306299df802637e4eeb6e1073
Falcobots-Team5457 <93661723+Team5457@users.noreply.github.com>
2021-11-12 09:35:24 -0500

    Create Wait.java

---
+++ Wait.java
@@ -0,0 +1,13 @@
+package frc.robot.nexuscommand;
+import frc.robot.*;
+
+public class Wait extends drive{
+
+
+    public Wait(double seconds, Driver driveSystem)
+    {
+        super(0,0,seconds,driveSystem);
+    }
+
+    
+}
